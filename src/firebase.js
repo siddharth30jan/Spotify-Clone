fc9c575184b6dd005b02191f82ed1f49b894054f
Siddharth Singh <siddharths51700@gmail.com>
2020-10-10 20:12:22 +0530

    Deployed!

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDzKPFC_KHfUpHfNX_u5AOiVJApTLd0o_Y",
+  authDomain: "spotify-clone-720bd.firebaseapp.com",
+  databaseURL: "https://spotify-clone-720bd.firebaseio.com",
+  projectId: "spotify-clone-720bd",
+  storageBucket: "spotify-clone-720bd.appspot.com",
+  messagingSenderId: "477623811642",
+  appId: "1:477623811642:web:46445214773e681ff5ff03",
+  measurementId: "G-YDL43WMXSW",
+});
--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,8 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "cf6426c4ec294ebc93cbbcb2f5d3cc0b";
-const redirectUri = "http://localhost:3000/";
+
+const redirectUri = "https://spotify-clone-720bd.web.app";
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
@@ -25,3 +26,4 @@ export const getTokenFromResponse = () => {
 export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
+//http://localhost:3000/#access_token=BQCBJbXOjlsIR1TF_GsAGseu3m08DZLK8Xc6TD1Qf_IzWB3_D9NagfPYkUOx9yBXuqXSkdXMzA84RbpnHTfum54vnEfVKH5Q-q-MX7ykvARcfg49-epJ2MKm8tJI76ozKYfxLzhHqSMXyaDeHtsVcc6PH1ijDL1Ss75QHwH-B13rGo_Ewrpa&token_type=Bearer&expires_in=3600
